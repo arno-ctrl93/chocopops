068c412509b0ed6c65c32e5dbfee5f657e5037cf
arnaud.notter <arnaud.notter@epita.fr>
2023-09-27 19:42:23 +0200

    :zap: done finally direction

--- game/player.js
+++ game/player.js
@@ -51,7 +51,7 @@ Player.prototype.displayInfo = function () {
 }
 
 Player.prototype.turnRight = function (angle) {
-    this.direction += angle;
+    this.direction -= angle;
     this.graphic.rotateOnAxis(new THREE.Vector3(0,0,-1), +angle);
 };
 
@@ -81,5 +81,5 @@ Player.prototype.move = function () {
     
     light1.position.x = this.position.x;
     light1.position.y = this.position.y;
-    //li ght1.position.z = this.graphic.position.z + 500;
+    //light1.position.z = this.graphic.position.z + 500;
 };
