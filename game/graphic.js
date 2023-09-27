e2f88264ef27fbf2c64abc04a7614e5d6252a6c2
arnaud.notter <arnaud.notter@epita.fr>
2023-09-27 20:10:35 +0200

    :zap: add enemy

--- game/graphic.js
+++ game/graphic.js
@@ -28,6 +28,9 @@ function init()
     player1 = new Player("player1", 0xffff00, new THREE.Vector2(50, 0), 0);
     scene.add(player1.graphic);
 
+    enemy1 = new Player("enemy1", 0xff0000, new THREE.Vector2(0, 0), 0);
+    scene.add(enemy1.graphic);
+
     console.log(player1.position.x + " " + player1.position.y);
 
     light1 = new Light("sun", 0xffffff, "0,0,355");
